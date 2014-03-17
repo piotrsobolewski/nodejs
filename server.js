e744e6f29c54d8943da464825991c6eca85d3d23
piotrsobolewski <piotr.sobolewski85@gmail.com>
2014-03-17 13:50:40 +0100

    new server file - simplicity

--- server.js
+++ server.js
@@ -6,38 +6,23 @@ var express = require('express')
 app.configure(function() {
 	app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);
   	app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+	app.set("transports", ["websocket"]);
 });
 
-// here we define the port the messages are sended though.
-// in this case, it is port 8080, you can define it to every port you want.
-//but take note, some ports are already in use, then it will throw in an error message.
 server.listen(app.get('port'), app.get('ipaddr'), function(){
 	console.log('Express server listening on  IP: ' + app.get('ipaddr') + ' and port ' + app.get('port'));
 });
-//a variable for our "automatch maing"
-TmpRoom="wait";
-//if a client connects to this, it performs this whole, big function.
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+/*
 io.sockets.on("connection", function (socket) {
 
-	// when the client emits 'adduser', this listens and executes
 	socket.on("joint", function(data){
-		// checks, if no other waiting player exists,
-		if(TmpRoom=="wait")
-		{
-			//if yes, then it perform this
-			// it sais TmpRoom = data, that is the username that is data
-			/*That means, TmpRoom is not anymore = wait! so the next one that
-			wants to join here, go into the next loop, the "else" loop */
-			TmpRoom = data;
-			//then it creates, and joins a room, that is called after the users username
-			socket.join(TmpRoom);
-			//Now it sends the message, you,playersname
-			socket.send("you," + TmpRoom);
-			// and now, it ends the loops.
-		}
-		
-		else
-		{
 			//Joins the Temporary Room, to the enemy player
 			socket.join(TmpRoom);
 			//Then it sends, the name of the room.
@@ -78,4 +63,4 @@ io.sockets.on("connection", function (socket) {
 		//on disconnecting, it leaves the room.
 		socket.leave(data);
 	});
-});
+});*/
